0d2f0c12cba450acd4f66c098d3ce208cdc84181
ivan-salazar14 <ivanssalazar14@gmail.com>
2026-01-23 08:03:00 -0500

    add domain

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'prana-market-production.up.railway.app'],
   },
 };
 
